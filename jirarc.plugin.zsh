efb14819a7b3be5e0c821c4d7ef34556b452d009
Anton Antov <105721104+aoantov@users.noreply.github.com>
2026-03-21 16:36:33 +0200

    fix(jirarc): Fix help (#15)
    * minor fix on help command
    
    * remove spacing

--- jirarc.plugin.zsh
+++ jirarc.plugin.zsh
@@ -1,12 +1,12 @@
- empty_user="x"
+empty_user="x"
 
- in_progress_status=$IN_PROGRESS_STATUS
- to_do_status=$TO_DO_STATUS
- blocked_status=$BLOCKED_STATUS
- review_status=$REVIEW_STATUS
- done_status=$DONE_STATUS
- qa_status=$QA_STATUS
- qa_user=$QA_USER
+in_progress_status=$IN_PROGRESS_STATUS
+to_do_status=$TO_DO_STATUS
+blocked_status=$BLOCKED_STATUS
+review_status=$REVIEW_STATUS
+done_status=$DONE_STATUS
+qa_status=$QA_STATUS
+qa_user=$QA_USER
 
 function jrua() {
   if [ -z "$1" ]; then
@@ -25,7 +25,6 @@ function jrrw() {
 
   jira issue move "$1" "$review_status"
 }
-
 
 function jram() {
   if [ -z "$1" ]; then
@@ -87,29 +86,29 @@ function jr() {
     - QA_STATUS v
     - QA_USER v
 
-
   Commands:
-  - jr/jrhelp: Show this help message
-  - jrme: List issues in the current sprint assigned to me
-  - jrime: List issues in the current sprint assigned to me (with details)
-  - jrall: List all issues assigned to me
-  - jrcat <issue-key>: View details of a specific issue
-  - jra <issue-key>: Assign an issue to a user
-  - jrmv <issue-key>: Move an issue to a different status
-  - jrrm <issue-key>: Remove (delete) an issue
-  - jric: Create a new issue
-  - jrics: List current sprint 
-  - jrcm <issue-key> <comment>: Add a comment to an issue
-  - jrcs: List issues in the current sprint
-  - jrop <issue-key>: Open an issue in the web browser
-  - jrua <issue-key>: Unassign an issue (assign to empty user)
-  - jram <issue-key>: Assign an issue to me
-  - jrrw(^) <issue-key>: Move an issue to 'Review' status
-  - jrtd(*) <issue-key>: Move an issue to 'To Do' status and unassign
-  - jrpr(+) <issue-key>: Move an issue to 'In Progress' status and assign to me
-  - jrdn(-) <issue-key>: Move an issue to 'Done'
-  - jrbk(&) <issue-key>: Move an issue to 'Blocked' status
-  - jrqa(v): Move an issue to 'QA' status and assign to QA user
+    - jrme: List issues in the current sprint assigned to me
+    - jrime: List issues in the current sprint assigned to me (interactive)
+    - jrall: List all issues assigned to me
+    - jriall: List all issues assigned to me (interactive)
+    - jrcat: \<issue-key\>: View details of a specific issue
+    - jra: \<issue-key\>: Assign an issue to a user
+    - jrmv: \<issue-key\>: Move an issue to a different status
+    - jrrm: \<issue-key\>: Remove (delete) an issue
+    - jric: Create a new issue
+    - jrcm: \<issue-key\> \<comment\>: Add a comment to an issue
+    - jrcs: List issues in the current sprint
+    - jrics: List issues in the current sprint (interactive)
+    - jrop: \<issue-key\>: Open an issue in the web browser
+    - jrua: \<issue-key\>: Unassign an issue (assign to empty user)
+    - jram: \<issue-key\>: Assign an issue to me
+    - jrrw: \<issue-key\>: Move an issue to 'Review' status
+    - jrbk: \<issue-key\>: Move an issue back to 'Blocked' status
+    - jrtd: \<issue-key\>: Move an issue to 'To Do' status and unassign
+    - jrpr: \<issue-key\>: Move an issue to 'In Progress' status and assign to me
+    - jrdn: \<issue-key\>: Move an issue to 'Done'
+    - jrqa: Move an issue to 'QA' status and assign to QA user
+    - jr/jrhelp: Show help for JiraRC commands
 EOF
 }
 
